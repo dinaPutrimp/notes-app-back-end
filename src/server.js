050f700f487851645ee70f4d06ee2f4d402f978b
Dina <127066162+equal-dina@users.noreply.github.com>
2025-07-12 06:31:31 +0700

    added joi validation

--- src/server.js
+++ src/server.js
@@ -1,7 +1,12 @@
 const Hapi = require("@hapi/hapi");
-const routes = require("./routes");
+const notes = require("./api/notes");
+const NotesService = require("./services/inMemory/NotesService");
+const NotesValidator = require("./validator/notes");
+const ClientError = require("./exceptions/ClientError");
 
 const init = async () => {
+  const notesService = new NotesService();
+
   const server = Hapi.server({
     port: 5000,
     host: process.env.NODE_ENV === "production" ? "0.0.0.0" : "localhost",
@@ -12,7 +17,26 @@ const init = async () => {
     },
   });
 
-  server.route(routes);
+  await server.register({
+    plugin: notes,
+    options: {
+      service: notesService,
+      validator: NotesValidator,
+    },
+  });
+
+  server.ext("onPreResponse", (request, h) => {
+    const { response } = request;
+
+    if (response instanceof ClientError) {
+      return h.response({
+        status: "fail",
+        message: response.message,
+      }).code(response.statusCode);
+    }
+
+    return h.continue;
+  });
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
